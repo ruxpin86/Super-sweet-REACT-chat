fe3f5b4e1dd88cd4acb2d17ed19e082bf003d3a9
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-03 12:58:32 -0600

    working on styling header

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h4>SUPER SWEET CHAT APP</h4>
+    </div>
+  );
+}
