27ee6dbdda2f8953a7d846855d8a8b746cbd7e0a
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-08-03 13:39:28 -0600

    building out login.js file

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -1,7 +1,85 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import Auth from "../../utils/auth";
+import { LOGIN_USER } from "../../utils/mutations";
+import { useMutation } from "@apollo/client";
+import { Collapse } from "react-collapse";
 
-export default function login() {
+export default function Login() {
+  const [loginFormData, setloginFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [open, setOpen] = useState(false);
+
+  const [login, { error }] = useMutation(LOGIN_USER);
+  if (error) {
+    console.log(JSON.stringify(error));
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setloginFormData({ ...loginFormData, [name]: value });
+  };
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    handleSubmit(async (submitData) => {
+      // console.log(submitData);
+      try {
+        const { data } = await login({
+          variables: { ...submitData },
+        });
+        Auth.login(data.login.token);
+        navigate("/chat");
+      } catch (err) {
+        console.error(err);
+      }
+
+      setloginFormData({
+        email: "",
+        password: "",
+      });
+    })(event);
+  };
+
   return (
-    <div>login</div>
-  )
+    <>
+      <h2 className="main-page-form" onClick={() => setOpen(!open)}>
+        Welcome Back! (Login)
+      </h2>
+      <Collapse isOpened={open}>
+        <br></br>
+        <form className="login-form">
+          <label>Email</label>
+          <input
+            {...register("email", { required: true })}
+            onChange={handleInputChange}
+          />
+          {errors.email && <p>Email is required</p>}
+          <label>Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true })}
+            onChange={handleInputChange}
+          />
+          {errors.password && <p>Password is required</p>}
+          <Link to="/main">
+            <button onClick={onSubmit} className="login-btn" type="submit">
+              Login
+            </button>
+          </Link>
+        </form>
+      </Collapse>
+    </>
+  );
 }
