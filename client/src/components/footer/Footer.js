839f4bab79ea0435e38da1d045826ae69058b08f
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-04 11:04:49 -0600

    added links to the footer and worked on incorporating hamburger menu

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./footer.css";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 
 export default function Footer() {
   return (
     <div className="footer-main">
-      <div>Andy</div>
-      <div>Ted</div>
+      <div className="credits">
+        <a href="https://github.com/andypieratt" target="blank">
+          <AiFillGithub size={15} />
+        </a>
+        <a href="https://www.linkedin.com/in/andrew-pieratt/" target="blank">
+          <AiFillLinkedin size={15} />
+        </a>
+        Andy P.
+      </div>
+      <div>© GoodGuysAlert Production</div>
+      <div className="credits">
+        <a href="https://github.com/ruxpin86" target="blank">
+          <AiFillGithub size={15} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/william-ted-glynn-71b269125"
+          target="blank"
+        >
+          <AiFillLinkedin size={15} />
+        </a>
+        Ted G.
+      </div>
     </div>
   );
 }
--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./header.css";
+import { Cross as Hamburger } from "hamburger-react";
 
 export default function Header() {
   return (
     <div className="header">
       <h4>SUPER SWEET CHAT APP</h4>
+      <div>{/* <Hamburger toggled={isOpen} /> */}</div>
     </div>
   );
 }
