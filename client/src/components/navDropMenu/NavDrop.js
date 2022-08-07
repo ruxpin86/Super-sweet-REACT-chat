36c128903f5736963c380078ca456172830f3353
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-07 14:07:44 -0600

    adding in nav components

--- client/src/components/navDropMenu/NavDrop.js
+++ client/src/components/navDropMenu/NavDrop.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./navDrop.css";
+import { useState } from "react";
 
-export default function NavDrop() {
+export default function NavDrop(props) {
   return (
-    <div>
+    <div className="nav">
       <nav className="nav-menu">
-        <ul>
-          <li>Dark/Light Mode</li>
-          <li>+New Convo</li>
-          <li></li>
+        <ul className="nav-links">
+          <li>
+            <a href="">Light/Dark Mode</a>
+          </li>
+          <li>
+            <a href="">+ New Convo</a>
+          </li>
+          <li>
+            <a href="">Another Feature</a>
+          </li>
         </ul>
       </nav>
     </div>
