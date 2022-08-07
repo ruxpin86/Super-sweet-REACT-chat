e986e6917d4e2dac24bf816a0e2fef17ba1388b7
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-08-07 13:08:15 -0600

    adding navigation for hamburger menu

---
+++ client/src/components/navDropMenu/NavDrop.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./navDrop.css";
+
+export default function NavDrop() {
+  return (
+    <div>
+      <nav className="nav-menu">
+        <ul>
+          <li>Dark/Light Mode</li>
+          <li>+New Convo</li>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
