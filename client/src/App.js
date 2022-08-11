617d873fa23fce00522d2a736407148ddb8d69dc
Andy Pieratt <andypieratt@Andys-MacBook-Pro.local>
2022-08-11 12:18:08 -0600

    trying to fix proxy issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-} from "@apollo/client";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+// } from "@apollo/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { setContext } from "@apollo/client/link/context";
 import Header from "./components/header/Header";
@@ -13,42 +13,42 @@ import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 import Chat from "./components/chat/Chat";
 
-const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
-});
+// const httpLink = createHttpLink({
+//   uri: "http://localhost:3001/graphql",
+// });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
+// const authLink = setContext((_, { headers }) => {
+//   const token = localStorage.getItem("id_token");
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : "",
+//     },
+//   };
+// });
 
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
+// const client = new ApolloClient({
+//   link: authLink.concat(httpLink),
+//   cache: new InMemoryCache(),
+// });
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <div>
-          <Header />
-          <div className="main">
-            {/* <Main /> */}
-            <Routes>
-              <Route exact path="/" element={<Main />} />
-              <Route exact path="/chat" element={<Chat />} />
-            </Routes>
-          </div>
-          <Footer />
+    // <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <Header />
+        <div className="main">
+          {/* <Main /> */}
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route exact path="/chat" element={<Chat />} />
+          </Routes>
         </div>
-      </Router>
-    </ApolloProvider>
+        <Footer />
+      </div>
+    </Router>
+    // </ApolloProvider>
   );
 }
 
